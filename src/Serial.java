cc7b38e4ae2391af3765c53df20541ac3a432f96
paul.klemstine <paul.klemstine@liveality.com>
2015-07-25 11:14:07 -0500

    socket recheck loop

--- src/Serial.java
+++ src/Serial.java
@@ -7,75 +7,128 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Serial {
-	
+
+	private static InputStream sin;
+
 	public static void main(String[] args) {
-			//while(port++<255){
-			try {
-				Socket socket = null;
+		// while(port++<255){
 		
-				System.out.print("opening socket ");
-					socket = new Socket("192.168.111.129", 81);
-					System.out.println("sock opened");
-					//socket.close();
-					//if (true)continue;
-					InputStream sin = socket.getInputStream();
-				
-				final OutputStream socketOutputStream = socket.getOutputStream();
+		try {
+			Socket socket = null;
 
-				new Thread(new Runnable() {
-					public void run() {
-						String line = "";
-						try {
-							BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("opening socket ");
+			socket = new Socket("192.168.111.129", 81);
+			System.out.println("sock opened");
+			// socket.close();
+			// if (true)continue;
+			sin = socket.getInputStream();
 
-							String input;
+			final OutputStream socketOutputStream = socket.getOutputStream();
 
-							while ((input = br.readLine()) != null) {
-								if (!input.startsWith("r"))
+			new Thread(new Runnable() {
+				public void run() {
+					String line = "";
+					try {
+						BufferedReader br = new BufferedReader(
+								new InputStreamReader(System.in));
+
+						String input;
+
+						while ((input = br.readLine()) != null) {
+							if (!input.startsWith("r"))
 								input += "5";
-								
-								//System.out.println(input);
-								for (int i = 0; i < input.length(); i++) {
-									socketOutputStream.write(input.charAt(i));
-									System.err.print(input.charAt(i));
-									try {
-										Thread.sleep(100);
-									} catch (InterruptedException e) {
-										e.printStackTrace();
-									}
-									socketOutputStream.flush();
 
+							// System.out.println(input);
+							for (int i = 0; i < input.length(); i++) {
+								socketOutputStream.write(input.charAt(i));
+								System.err.print(input.charAt(i));
+								try {
+									Thread.sleep(500);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
 								}
-								//System.out.println(line);
+								socketOutputStream.flush();
+
 							}
-
-						} catch (IOException io) {
-							io.printStackTrace();
+							// System.out.println(line);
 						}
-					}
-				}).start();
 
-				new Thread(new Runnable() {
-					public void run() {
-						String line = "";
-						BufferedReader in = null;
-						in = new BufferedReader(new InputStreamReader(sin));
-						while (true) {
+					} catch (IOException io) {
+						io.printStackTrace();
+					}
+				}
+			}).start();
+
+			new Thread(new Runnable() {
+				public void run() {
+					String line = "";
+					BufferedReader in = null;
+					in = new BufferedReader(new InputStreamReader(sin));
+					while (true) {
+						try {
+							if ((line = in.readLine()) != null) {
+								System.out.println(":" + line);
+							}
+						} catch (IOException e) {
+							while(true){
+							System.out.println(" error");
+							
+							Socket socket = null;
+							System.out.print("opening socket ");
 							try {
-								if ((line = in.readLine()) != null) {
-									System.out.println(":" + line);
-								}
-							} catch (IOException e) {
+								socket = new Socket("192.168.111.129", 81);
+							} catch (UnknownHostException e2) {
+								e.printStackTrace();
+							} catch (IOException e2) {
 								e.printStackTrace();
 							}
+							if (socket != null) {
+								System.out.println("sock opened");
+								try {
+									sin = socket.getInputStream();
+								} catch (IOException e2) {
+									e.printStackTrace();
+								}
+								break;
+							}
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e2) {
+								e.printStackTrace();
+							}
+							}
 						}
 					}
-				}).start();
-			} catch (Exception gghe) {
-				gghe.printStackTrace();
-				System.out.println(" error");
+				}
+			}).start();
+		} catch (Exception gghe) {
+			while(true){
+			System.out.println(" error");
+			Socket socket = null;
+
+			System.out.print("opening socket ");
+			try {
+				socket = new Socket("192.168.111.129", 81);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (socket != null) {
+				System.out.println("sock opened");
+				try {
+					sin = socket.getInputStream();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			}
 		}
-	//}	
-		
+	}
 }
